51bac77ed1d9dab4ec0dba452ecdfc75b7cacf6c
Swann HERRERA <swann@devloup.dev>
2022-09-05 23:14:44 +0200

    configure lsp and null-ls and other plugins

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,9 +1,21 @@
 return {
+  ["github/copilot.vim"] = {},
+  ["mfussenegger/nvim-dap"] = {},
   ["neovim/nvim-lspconfig"] = {
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.plugins.lspconfig"
     end,
+  },
+  ["theHamsta/nvim-dap-virtual-text"] = {
+    config = function ()
+      require("nvim-dap-virtual-text").setup()
+    end
+  },
+  ["rcarriga/nvim-dap-ui"] = {
+    config = function ()
+      require("dapui").setup()
+    end
   },
   ["max397574/better-escape.nvim"] = {
     event = "InsertEnter",
@@ -23,5 +35,12 @@ return {
       })
     end
   },
+  ["ThePrimeagen/harpoon"] = {},
+  ["jose-elias-alvarez/null-ls.nvim"] = {
+        after = "nvim-lspconfig",
+        config = function()
+           require "custom.plugins.null-ls"
+        end,
+  }
 }
 
